6e70363247a5f9059626b1bf436b37007fc91d76
Q118 <shelbyfish91@gmail.com>
2021-01-31 08:28:06 -0800

    set up local p5.js variable locally

---
+++ sketch.js
@@ -0,0 +1,9 @@
+/** @format */
+
+function setup() {
+	createCanvas(400, 400);
+}
+function draw() {
+	background(120);
+ }
+// We create the canvas on the method setup() because it runs only once the program gets loaded the first time. The method draw() keeps running till the program ends, and there we define the code wanted to render on the screen. 
